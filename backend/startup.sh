136f04aa812da169d256d3324b47888e5228c5d9
solhaile <solhailet@gmail.com>
2025-07-06 12:08:39 -0700

    Update startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Install dependencies if node_modules doesn't exist
+# Install dependencies if node_modules doesn't exist in the zip folder
 if [ ! -d "node_modules" ]; then
     echo "📦 Installing dependencies..."
     npm install --production
 fi
 
-# Start the application
+# Start the application.
 echo "🚀 Starting application..."
 node server.js
